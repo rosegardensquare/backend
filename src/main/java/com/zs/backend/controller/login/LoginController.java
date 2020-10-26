27390710c0afc276bd3e4dd5bcebc3d55d2eccf9
2548528021@qq.com <2548528021@qq.com>
2020-10-26 22:24:09 +0800

    提交

--- src/main/java/com/zs/backend/controller/login/LoginController.java
+++ src/main/java/com/zs/backend/controller/login/LoginController.java
@@ -1,19 +1,31 @@
 package com.zs.backend.controller.login;
 
+import com.alibaba.fastjson.JSON;
+import com.zs.backend.entity.User;
+import com.zs.backend.model.UserModel;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class LoginController {
 
-    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    //private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping("login")
-    public void login() {
-        logger.info("------");
+    public void login(@RequestBody User userModel) {
+        log.info("------" + JSON.toJSONString(userModel));
+        String a = "{'userName':'1'}";
+
+
+        User u = JSON.parseObject(a, UserModel.class);
+        log.info("------" + JSON.toJSONString(u));
     }
 
 }
---
+++ src/main/java/com/zs/backend/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.zs.backend.model;
+
+import com.zs.backend.entity.User;
+
+public class UserModel extends User {
+
+}
