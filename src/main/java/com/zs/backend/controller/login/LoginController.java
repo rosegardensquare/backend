f608f97ede1d02da9e7ad6a32d08c2b0386e4c29
2548528021@qq.com <2548528021@qq.com>
2020-10-26 22:27:24 +0800

    测试提交

--- src/main/java/com/zs/backend/controller/login/LoginController.java
+++ src/main/java/com/zs/backend/controller/login/LoginController.java
@@ -19,6 +19,7 @@ public class LoginController {
     //private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 
+
     @RequestMapping("login")
     public void login(@RequestBody User userModel) {
         log.info("------" + JSON.toJSONString(userModel));
