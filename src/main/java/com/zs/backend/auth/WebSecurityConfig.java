f1f48b3c685ef2e4380cb0b0755ca11c6ffaa678
zhangshuai <13674987759@163.com>
2021-01-27 15:14:42 +0800

    no msg

--- src/main/java/com/zs/backend/auth/WebSecurityConfig.java
+++ src/main/java/com/zs/backend/auth/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.zs.backend.security.handler.AuthenticationFailureHandlerImpl;
 import com.zs.backend.security.handler.AuthenticationSuccessHandlerImpl;
 import com.zs.backend.sys.auth.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,9 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import org.springframework.context.annotation.Bean;
-
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 
