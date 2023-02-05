155db1fcfe5c09adebbdda17dc591ce0d2713d1c
zhangshuai <zs199112>
2023-02-05 20:25:35 +0800

    生产者发送消息成功，消费者也能监听到消息

--- src/main/java/com/zs/backend/test/ConsumeListener.java
+++ src/main/java/com/zs/backend/test/ConsumeListener.java
@@ -1,9 +1,6 @@
 package com.zs.backend.test;
 
-import com.rabbitmq.client.Channel;
 import com.zs.backend.config.RabbitMQConfig;
-import java.io.IOException;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +8,7 @@ import org.springframework.stereotype.Component;
 public class ConsumeListener {
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE)
-    public void consume(String msg) throws IOException {
+    public void consume(String msg) {
         System.out.println("队列的消息为：" + msg);
-//        String correlationId = message.getMessageProperties().getCorrelationId();
-//        System.out.println("唯一标识为：" + correlationId);
-//        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
     }
 }
