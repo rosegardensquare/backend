b8b49d79ddf01cef47d4a04229f5f96616a36d2b
zhangshuai <zs199112>
2023-02-06 22:35:58 +0800

    设置消息持久化成功

--- src/main/java/com/zs/backend/test/rabbitmq/RabbitMQTestController.java
+++ src/main/java/com/zs/backend/test/rabbitmq/RabbitMQTestController.java
@@ -6,7 +6,9 @@ import com.zs.backend.config.RabbitMQConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback;
@@ -84,6 +86,25 @@ public class RabbitMQTestController {
             public Message postProcessMessage(Message message) throws AmqpException {
                 // 设置消息的唯一标识
                 message.getMessageProperties().setCorrelationId("123");
+                return message;
+            }
+        });
+        return Result.result(JSON.toJSON(rabbitTemplate));
+    }
+
+    /**
+     * 队列中消息持久化
+     * 设置 deliveryMode 为 2
+     * @return
+     */
+    @GetMapping("/deliveryMode")
+    public Result deliveryMode(){
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE,
+            RabbitMQConfig.ROUTING_KEY, "rabbitMQ。。。。。。", new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                // 设置消息的唯一标识
+                message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
                 return message;
             }
         });
