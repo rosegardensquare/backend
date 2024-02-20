d95f857dceaa41147603738e1d20483c2badc9f7
zhangshuai <zhangshuai>
2024-02-20 11:25:11 +0800

    springboot 项目引入其他项目依赖

--- backend-common/src/main/java/com/zs/backend/utils/Test.java
+++ backend-common/src/main/java/com/zs/backend/utils/Test.java
@@ -2,12 +2,14 @@ package com.zs.backend.utils;
 
 
 import com.zs.common.utils.CommonDateUtil;
+import com.zs.common.utils.TestUtil;
 
 public class Test {
 
     public static void main(String[] args) {
         String defaultFormat = CommonDateUtil.DEFAULT_FORMAT;
         System.out.println(defaultFormat);
+        TestUtil.test();
     }
 
 }
