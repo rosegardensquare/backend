821e04e147f9574698e0a7ab2157742fa5766390
zhangshuai <zhangshuai>
2024-02-20 15:54:40 +0800

    批量查询数据

---
+++ backend-service/src/main/java/com/zs/backend/operate/BatchQueryProcess.java
@@ -0,0 +1,29 @@
+package com.zs.backend.operate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface BatchQueryProcess {
+
+    public static final Integer batchSize = 1000;
+
+    /**
+     * 分批查询数据
+     */
+//    private void batchQuery(){
+//        Long minId = 0L;
+//        List<Long> orderIds = new ArrayList<>();
+//        while (true){
+//            List orders = orderService.queryByIds(minId, batchSize);
+//            if(orders != null || orders.size() == 0){
+//                break;
+//            }
+//            orderIds.addAll(orders.stream().map(OrderDTO::getId).collect(Collectors.toList()));
+//            minId = orderIds.get(orderIds.size() -1);
+//        }
+//    }
+
+}
